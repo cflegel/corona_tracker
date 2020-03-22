55434bb208ae8688900ef7695347fc589410eac7
Christoph Flegel <c.flegel@a-b-one-digital.de>
2020-03-22 07:50:52 +0100

    Switch to ListView to avoid vertical overflow.

--- frontend/corona_tracker/lib/navigation/StatusScreen.dart
+++ frontend/corona_tracker/lib/navigation/StatusScreen.dart
@@ -9,90 +9,99 @@ class StatusScreen extends StatelessWidget {
         title: Text(AppLocalizations.of(context).StatusScreen_AppBarTitleText),
       ),
       body: Center(
-          child: Column(
-        children: [
-          new Container(
-            child: Image.asset(
-              'assets/images/corona_tracker_homescreen_teaser.png',
-              fit: BoxFit.cover,
+        child: ListView(
+          children: [
+            Container(
+              child: Image.asset(
+                'assets/images/corona_tracker_homescreen_teaser.png',
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-          ),
-          Column(
-            children: [
-              Icon(
-                Icons.people,
-                color: Colors.blueGrey,
-                size: 50.0,
-              ),
-              Text('Anzahl der Benutzer', style: TextStyle(fontSize: 34)),
-              //TODO API CALL
-              Text(
-                '13022',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  decoration: TextDecoration.underline,
-                  fontSize: 34,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.people,
+                  color: Colors.blueGrey,
+                  size: 50.0,
                 ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Icon(
-                Icons.person,
-                color: Colors.green[500],
-                size: 50.0,
-              ),
-              Text(
-                'Eigener Status',
-                style: TextStyle(
-                  //fontWeight: FontWeight.bold,
-                  fontSize: 34,
+                // TODO i18n
+                Text(
+                  'Anzahl der Benutzer',
+                  style: TextStyle(
+                    fontSize: 34,
+                  ),
                 ),
-              ),
-              //TODO API CALL
-              Text(
-                'Gesund',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 34,
-                  decoration: TextDecoration.underline,
+                // TODO API CALL
+                Text(
+                  '13022',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                    fontSize: 34,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Icon(
-                Icons.healing,
-                color: Colors.green[500],
-                size: 50.0,
-              ),
-              Text(
-                'Anzahl der Geheilten',
-                style: TextStyle(
-                  //fontWeight: FontWeight.bold,
-                  fontSize: 34,
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.person,
                   color: Colors.green[500],
+                  size: 50.0,
                 ),
-              ),
-              //TODO API CALL
-              Text(
-                '13022',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 34,
-                  decoration: TextDecoration.underline,
+                // TODO i18n
+                Text(
+                  'Eigener Status',
+                  style: TextStyle(
+                    //fontWeight: FontWeight.bold,
+                    fontSize: 34,
+                  ),
+                ),
+                // TODO API CALL
+                Text(
+                  'Gesund',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 34,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(
+                  Icons.healing,
                   color: Colors.green[500],
+                  size: 50.0,
                 ),
-              ),
-            ],
-          ),
-        ],
-      )),
+                // TODO i18n
+                Text(
+                  'Anzahl der Geheilten',
+                  style: TextStyle(
+                    //fontWeight: FontWeight.bold,
+                    fontSize: 34,
+                    color: Colors.green[500],
+                  ),
+                ),
+                // TODO API CALL
+                Text(
+                  '13022',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 34,
+                    decoration: TextDecoration.underline,
+                    color: Colors.green[500],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
